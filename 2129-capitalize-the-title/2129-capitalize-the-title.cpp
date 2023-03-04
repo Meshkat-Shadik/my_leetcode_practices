b553896c302a4ff12e405394d9757258cd506059
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-03-04 21:31:23 +0600

    Time: 2 ms (68.22%), Space: 6.2 MB (93.36%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.cpp
+++ 2129-capitalize-the-title/2129-capitalize-the-title.cpp
@@ -7,11 +7,13 @@ public:
             int temp = i;     //first index
             while(t[i]!=' ' && i<len)
             {
-               t[i]= tolower(t[i]); 
+              // t[i]= tolower(t[i]);
+               t[i] = t[i] | 32;
                i++;           //finally last index of the word
             }
             if(i-temp>2){  // last - first length > 2 then it is a word
-                t[temp] = toupper(t[temp]);  // turn first char upper
+                //t[temp] = toupper(t[temp]);  // turn first char upper
+                t[temp] = t[temp] & ~32;
             }
         }
         return t;
