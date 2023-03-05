b046a239b85520131f09eb6e8792192b7d667fe9
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-03-05 20:10:00 +0600

    Time: 0 ms (100.00%), Space: 6.1 MB (56.50%) - LeetHub

--- 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int percentageLetter(string s, char l) {
-        unordered_map<char,int>m;
         int len = s.size();
+        int count = 0;
         for(int i=0;i<len;i++)
         {
-            m[s[i]]++;
+            if(s[i]==l)
+            {
+                count++;
+            }
         }
-        //cout<<m[l]<<" - "<<(m[l]*100)/len<<endl;
-        return (m[l]*100)/len;
+        return (count*100)/len;
     }
 };
