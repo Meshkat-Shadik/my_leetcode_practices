71605c718c05a0f45cce069c2c94257a20ae8002
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-03-06 22:03:06 +0600

    Time: 3 ms (82.97%), Space: 6.9 MB (88.45%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.cpp
+++ 2325-decode-the-message/2325-decode-the-message.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     string decodeMessage(string k, string me) {
-        unordered_map<char,char>m;
+        vector<int>v(26,-1);
         int len1 = k.size();
         int len2 = me.size();
-      char temp = 'a';
+      int temp = 0;  
       for(int i=0; i<len1; i++)
       {
-          if(k[i] != ' ' && m.find(k[i]) == m.end())
+          if(k[i] != ' ' && v[k[i]-'a']==-1)
           {
-              m[k[i]]=temp;
+              v[k[i]-'a'] = temp;
               temp++;
           }
       }
-      k.clear();
-      for(int i=0; i<len2; i++)
-      {
-          if(me[i] == ' ')
-          {
-              k = k+" ";
-          }
-          else
-          {
-              k = k+ m[me[i]];
-          }
-      }
-        return k;
+        
+        for(int i=0;i<len2; i++)
+        {
+            if(me[i]!=' ')
+            {
+                me[i]=v[me[i]-'a']+'a';
+            }
+        }
+        return me;
     }
 };
