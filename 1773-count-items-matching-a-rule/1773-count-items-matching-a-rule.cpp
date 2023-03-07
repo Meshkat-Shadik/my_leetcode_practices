d9d10746fb6fe43706dabc98c7a7bd04127a9304
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-03-07 19:07:38 +0600

    Time: 78 ms (69.32%), Space: 31 MB (39.94%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
@@ -3,11 +3,11 @@ public:
     int countMatches(vector<vector<string>>& it, string rK, string rV) {
        
         int index = 0;
-        if(rK == "color")
+        if(rK[0] == 'c') //trick for runtime fast
         {
             index = 1;
         }
-        else if(rK == "name"){
+        else if(rK[0] == 'n'){
             index = 2;
         }
         int len = it.size();
