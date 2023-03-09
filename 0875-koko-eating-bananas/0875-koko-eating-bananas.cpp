c290067461b5f7becdba5fb4bd3d1d1ebf2ec21b
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-03-09 10:19:40 +0600

    Time: 55 ms (50.86%), Space: 19 MB (11.66%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -6,26 +6,31 @@ public:
         int ans = 0;
         while(low<=high){
             int mid = low + (high-low)/2;
-            //cout<<"Mid = "<<mid<<endl;
-            long time = work(piles,mid);
-           // cout<<time<<endl;
-            if(time <= h)
+            long check = canEat(piles,mid,h);
+            if(check)
             {
-                ans = mid;
                 high = mid -1;
             }
             else{
                 low = mid+1;
             }
         }
-        return ans;
+        return low;
     }
-    long work(vector<int>& v, int div){
+    bool canEat(vector<int>& p,int m, int h)
+    {
+        int len = p.size();
         long sum = 0;
-        for(int i=0;i<v.size(); i++)
+        
+        for(int i=0; i<len ; i++)
         {
-            sum+=(v[i]/div)+((v[i]%div)!=0);
+            int res = p[i]/m;
+            sum = sum+res;
+            if(p[i]%m != 0)
+            {
+                sum++;
+            }
         }
-        return sum;
+        return sum<=h;
     }
 };
