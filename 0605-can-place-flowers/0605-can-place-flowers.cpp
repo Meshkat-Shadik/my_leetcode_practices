df206bf0856d06c84fa8ce4e9190fa44f36c5e02
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-03-20 12:12:12 +0600

    Time: 20 ms (53.38%), Space: 20.3 MB (34.30%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -2,12 +2,18 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& f, int n) {
         int len = f.size();
-         for(int i=0;i<len && n>0;i++){
-         if(f[i]==0 &&(i==0 || f[i-1]==0) &&(i==len-1 || f[i+1]==0) ){
-            n--;
-            i++;
-        } 
-    }
-    return n==0?true:false;
+        int z = 1, ans = 0;  //zero initialized as 1 for prefix extra 0
+        for(int i=0; i<len; i++)
+        {
+            if(f[i]==0){
+                z++;
+            }
+            else
+            {
+                ans = ans+(z-1)/2;
+                z = 0;
+            }
+        }
+        return ans+(z/2) >= n;    //last one doesn't need -1
     }
 };
