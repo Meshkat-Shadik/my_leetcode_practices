baaccd90b1a9953099756fe6bc4b1bf1c6c83ac2
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-03-22 00:14:05 +0600

    Time: 7 ms (93.52%), Space: 8.1 MB (64.11%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     bool judgeCircle(string m) {
-        int initX = 0;
-        int initY = 0;
+        int up = 0, down = 0, left = 0, right = 0;
         int len = m.size();
         
         for(int i=0;i<len;i++)
         {
-            if(m[i]=='R')
-            {
-                initX++;
-            }
-            if(m[i]=='L')
-            {
-                initX--;
-            }
-            if(m[i]=='U')
-            {
-                initY++;
-            }
-            if(m[i]=='D')
-            {
-                initY--;
-            }
+           if(m[i]=='R') right++;
+           else if(m[i]=='L') left++;
+           else if(m[i]=='U') up++;
+           else if(m[i]=='D') down++;
         }
-        return initX == 0 && initY == 0;
+        return right == left && up == down;
     }
 };
