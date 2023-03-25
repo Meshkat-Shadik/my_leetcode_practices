474ac6680f530957b7402e5fefef81b532411a90
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-03-25 15:09:00 +0600

    Time: 349 ms (90.56%), Space: 108.8 MB (66.01%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
-    vector<int>g[50005];
-    bool vis[50005];
-    set<pair<int,int>>s;
-    int minReorder(int n, vector<vector<int>>& con) {
-        memset(vis,false,sizeof(vis));
-        int len = con.size();
-        for(int i=0; i<len; i++)
-        {
-            g[con[i][0]].push_back(con[i][1]);
-            g[con[i][1]].push_back(con[i][0]);
-            s.insert({con[i][0],con[i][1]});
-        }
-        queue<int>q;
-        q.push(0);
-        int count = 0;
-        while(!q.empty()){
-            int top = q.front();
-            q.pop();
-            if(!vis[top]){
-                vis[top] = true;
-                for(int i=0; i<g[top].size();i++)
-                {
-                    q.push(g[top][i]);
-                    if(!vis[g[top][i]] && s.count({top,g[top][i]})){
-                        count++;
-                    }
+    struct e{
+        int to = 0;
+        bool exist = false;
+    };
+    
+    vector<vector<e>>g;
+    vector<int>vis;
+    int count = 0;
+    
+    
+    
+    void dfs(vector<vector<int>>& con, int node){
+        vis[node] = 1;
+        for(auto i:g[node]){
+            if(vis[i.to] == 0)   //not visited
+            {
+                if(i.exist){   //check reversed direction
+                    count++;
                 }
+                dfs(con,i.to);
             }
         }
+    }
+    
+    
+    int minReorder(int n, vector<vector<int>>& con) {
+        vis.resize(n);
+        g.resize(n);
+        int len = con.size();
+        for(int i=0; i<len;i++){
+            g[con[i][0]].push_back({con[i][1],true}); //reversed direction true
+            g[con[i][1]].push_back({con[i][0],false});
+        }
+        dfs(con,0);
         return count;
     }
 };
