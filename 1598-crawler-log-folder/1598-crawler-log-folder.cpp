ae0336b8a95499bf06239f576d0ef5caf85325e4
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-04-11 13:01:54 +0600

    Time: 7 ms (59.55%), Space: 10.4 MB (90.91%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(vector<string>& s) {
+        int len = s.size();
+        int count = 0;
+        for(int i=0 ; i<len ; i++){
+            if(s[i][1]=='.'){  // sure it is ../
+                if(count>0){
+                    count--;
+                }
+                //cout<<s[i]<<" so -- , "<<count<<endl;
+            }
+            else if(s[i][1] == '/' && s[i][0] == '.'){  //sure it is ./
+                continue;
+              //  cout<<s[i]<<" skiped , "<<count<<endl;
+            }
+            else{
+                count++;
+                //cout<<s[i]<<" so ++ , "<<count<<endl;
+            }
+        }
+        return count;
+    }
+};
