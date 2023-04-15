f846a82d3126a74752f7366225170b35dc105584
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-04-16 04:14:02 +0600

    Time: 0 ms (100.00%), Space: 7.9 MB (72.88%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        vector<string> result;
-        for(int i=1; i<=n; i++){
-            result.push_back(to_string(i));
-        }
-        for(int i=2; i<n; i=i+3){
-            result[i] = "Fizz";
-        }
-        for(int i=4; i<n; i=i+5){
-            result[i] = "Buzz";
-        }
-        for(int i=14; i<n; i=i+15){
-            result[i] = "FizzBuzz";
-        }
-        return result;
+      vector<string>ans;
+       for(int i=1;i<=n;i++){
+           if(i%15==0){
+               ans.push_back("FizzBuzz");
+           }
+           else if(i%3==0)ans.push_back("Fizz");
+           else if(i%5==0)ans.push_back("Buzz");
+           else ans.push_back(to_string(i));
+       }
+       return ans;
     }
 };
