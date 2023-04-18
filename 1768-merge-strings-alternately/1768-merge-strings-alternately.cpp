41457f42cfccd293f179aa47a04eec4e4e98ebbe
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-04-18 12:48:47 +0600

    Time: 0 ms (100.00%), Space: 6.2 MB (98.18%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     string mergeAlternately(string w1, string w2) {
         int l1 = w1.size(), l2 = w2.size();
-        int len = max(l1,l2);
+        int i=0,j=0;
         string res;
-        for(int i=0; i<len; i++){
-            if(i<l1){
-                res = res + w1[i];
-            }
-            if(i<l2){
-                res = res + w2[i];
-            }
+        
+        while(i<l1 && j<l2){
+            res.push_back(w1[i++]);
+            res.push_back(w2[j++]);
+        }
+        while(i<l1){
+            res.push_back(w1[i++]);
+        }
+         while(j<l2){
+            res.push_back(w2[j++]);
         }
         return res;
     }
