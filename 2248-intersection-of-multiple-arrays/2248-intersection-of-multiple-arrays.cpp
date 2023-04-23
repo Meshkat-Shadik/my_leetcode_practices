f0a82c43539593bdf85c3d300671afdf55b85281
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-04-24 01:51:23 +0600

    Time: 13 ms (63.17%), Space: 12.6 MB (50.27%) - LeetHub

---
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> intersection(vector<vector<int>>& nums) {
+        map<int,int>m;
+        int l1 = nums.size();
+        
+        for(int i=0; i<l1; i++){
+            for(int j=0; j<nums[i].size(); j++){
+                m[nums[i][j]]++;
+            }
+        }
+        
+        vector<int>ans;
+        for(auto i:m){
+            if(i.second == l1){
+                ans.push_back(i.first);
+            }
+        }
+        return ans;
+    }
+};
