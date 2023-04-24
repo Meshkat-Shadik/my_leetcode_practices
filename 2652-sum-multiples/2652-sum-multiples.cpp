cd00eee23a46a6128839789256a5b6495233678c
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-04-24 15:07:15 +0600

    Time: 6 ms (27.27%), Space: 5.9 MB (45.45%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.cpp
+++ 2652-sum-multiples/2652-sum-multiples.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
+    int sum(int n, int x){
+         return x*((n/x)*((n/x)+1)/2);
+    }
+    
     int sumOfMultiples(int n) {
-           int ans = 0;
-        for(int i = 3; i <= n; i++){
-            if(i%3==0||i%5==0||i%7==0){
-                ans += i;
-            }
-        }
-        return ans;
+       int t = sum(n,3);
+       int f = sum(n,5);
+       int s = sum(n,7);
+       int tf = sum(n,15);
+       int fs = sum(n,35);
+       int ts = sum(n,21);
+       int tfs = sum(n,105);
+        
+      return t+f+s-tf-fs-ts+tfs;
     }
 };
