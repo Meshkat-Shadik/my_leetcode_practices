855d8336267788610d855ad0a6e5f965f377df3a
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-04-26 11:51:59 +0600

    Time: 0 ms (100.00%), Space: 6.1 MB (7.98%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int addDigits(int num) {
-        if(num<10) return num;
-        else if(num%9 == 0){
+        if(num && num%9 == 0){
             return 9;
         }else{
             return num%9;
         }
-        return num;
     }
 };
