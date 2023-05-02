a504151ead63a5bc3cb3780fa250cd47dce1e2ad
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-05-02 08:52:08 +0600

    Time: 8 ms (27.40%), Space: 10.2 MB (86.28%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int negC = 0;
+        int siz = nums.size();
+        for(int i=0; i<siz; i++){
+            if(nums[i]==0){
+                return 0;
+            }
+            if(nums[i]<0){
+                negC++;
+            }
+        }
+        if((negC&1)==0){
+            return 1;
+        }
+        else{
+            return -1;
+        }
+        
+    }
+};
