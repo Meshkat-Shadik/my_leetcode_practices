091baa5577d352487d5ffc88d6201968e8a6c389
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-05-08 10:28:50 +0600

    Time: 22 ms (5.66%), Space: 11.2 MB (64.67%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-         int len = mat.size();
+    int len = mat.size();
     int sum = 0;
       for(int i=0; i<len;i++)
       {
           sum = sum + (mat[i][i]); //primary
           sum = sum + (mat[len-1-i][i]); //secondary
       }
-      if(len%2 == 0){
-          return sum;
+      if(len&1){  //odd
+          return sum - mat[len/2][len/2];
       }
-      else{
-        return sum - mat[len/2][len/2];      
-      }
+      return sum;
     }
 };
