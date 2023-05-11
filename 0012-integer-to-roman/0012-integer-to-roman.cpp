f642597f2f6515833ea0cdc6647c6a52ecffadd0
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-05-11 16:32:31 +0600

    Time: 8 ms (66.10%), Space: 5.9 MB (94.08%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,21 +1,16 @@
 class Solution {
-    string table[4][10] = {{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
-                           {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
-                           {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
-                           {"", "M", "MM", "MMM"}
-                          };
 public:
     string intToRoman(int num) {
-        // 
-        int count = 0;
-        string ans;
-        while(num>0)
-        {
-            int fDigit = num%10;
-            num = num/10;
-            ans = table[count][fDigit]+ans;
-            count++;
+        int roman[13] = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
+        string code[13]={"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+        string res="";
+        
+        for(int i=12;i>=0;i--){
+            while(num>=roman[i]){
+                res = res + code[i];
+                num = num - roman[i];
+            }
         }
-        return ans;
+        return res;
     }
 };
