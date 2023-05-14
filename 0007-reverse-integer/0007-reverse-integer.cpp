85dfaf7ed1cee5d69d07b13a3aa35dfc1c05e0a6
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-05-14 19:02:37 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (93.39%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-    int reverse(int x) {
-        int ans = 0;
-        while(x){
-            int dig = x%10;
-            
-            if(ans > (INT_MAX/10 - dig/10) || ans < (INT_MIN/10 - dig/10)){
-                return 0;
-            }
-            ans = ans * 10;
-            ans = ans + dig;
-            x = x/10;
-        }
+    int reverse(int n) {
+    int ans=0;
+        while(n)
+        {
+            if((ans>INT_MAX/10) || (ans<INT_MIN/10)) return 0;
+            ans=(ans*10)+(n%10);
+            n/=10;
+        } 
         return ans;
     }
 };
