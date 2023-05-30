4cf238a0085db0ea5ae7b757ff566cd1bd1c1d26
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-05-30 13:37:39 +0600

    Time: 16 ms (81.61%), Space: 51.8 MB (9.82%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.java
@@ -0,0 +1,9 @@
+class MyHashSet extends HashSet<Integer> {}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
