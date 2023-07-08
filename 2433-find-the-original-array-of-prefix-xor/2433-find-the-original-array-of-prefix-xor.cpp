58c349911b55fd6e65e906ee4531dddc7b519c6a
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-07-08 19:57:03 +0600

    Time: 154 ms (9.75%), Space: 75.9 MB (75.77%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<int> findArray(vector<int>& pref) {
-        vector<int>ans;
-        ans.push_back(pref[0]);
+        int xorV = pref[0];
         for(int i=1;i<pref.size();i++){
-            ans.push_back(pref[i-1]^pref[i]);
+            int tmp = xorV ^ pref[i];
+            pref[i] = tmp;
+            xorV = xorV ^ pref[i];
         }
-        return ans;
+        return pref;
     }
 };
