f9c0e77ca440c9128bdace79aa37028994079afb
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-08-17 13:47:41 +0600

    Time: 5 ms (32.79%), Space: 8.2 MB (70.36%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-      unordered_map<int,int>m;
+      unordered_map<int,int>m1,m2;
         for(int i=0; i<arr.size();i++){
-            m[arr[i]]++;
+            m1[arr[i]]++;
         }
-    unordered_set<int>s;
-        for(auto &i : m){
-            if(!s.insert(i.second).second) return false;
+        for(auto &i : m1){
+            if(m2[i.second]){
+                return false;
+            }
+            else{
+                m2[i.second]=1;
+            }
         }
         return true;
     }
