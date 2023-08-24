d5c884755c17d63f0a9cc4d967244c79b377668c
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-08-24 16:07:43 +0600

    Time: 3 ms (85.99%), Space: 6.4 MB (77.87%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.cpp
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -2,14 +2,29 @@ class Solution {
 public:
     string finalString(string s) {
         string ans;
-        for(int i=0; i<s.size();i++){
+        int len = s.length();
+        for(int i=0; i<len;i++){
             if(s[i]=='i'){
-                reverse(begin(ans), end(ans));
-            }
-            else{
-                 ans = ans + s[i];
+                rev(0,i-1,s);
             }
         }
+        //complete the previous swapping all things 
+        //then just check which are not i push them to ans
+        for(int i=0; i<len; i++){
+            if(s[i] != 'i'){
+                ans.push_back(s[i]);
+            }
+        }
+        
         return ans;
     }
+    
+    void rev(int start, int end, string& s){
+        while(start<=end){
+            swap(s[start],s[end]);
+            start++;
+            end--;
+        }
+    }
+    
 };
