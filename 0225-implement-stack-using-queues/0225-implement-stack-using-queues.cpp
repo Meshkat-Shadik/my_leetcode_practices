0c0fbbfba4d1425de5dc2b068e46db936a84936e
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-08-28 17:36:32 +0600

    Time: 0 ms (100.00%), Space: 6.7 MB (67.22%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,31 +1,30 @@
 class MyStack {
 public:
-    queue<int>q1, q2;
+    queue<int>q;
     MyStack() {
         
     }
     
     void push(int x) {
-        q2.push(x);
-        while(!q1.empty()){
-            q2.push(q1.front());
-            q1.pop();
-        }
-        swap(q1,q2);
+       q.push(x);
+       for(int i=0 ;i<q.size()-1 ; i++){
+           q.push(q.front());
+           q.pop();
+       }
     }
     
     int pop() {
-        int top = q1.front();
-        q1.pop();
+        int top = q.front();
+        q.pop();
         return top;
     }
     
     int top() {
-        return q1.front();
+        return q.front();
     }
     
     bool empty() {
-        return q1.empty();
+        return q.empty();
     }
 };
 
