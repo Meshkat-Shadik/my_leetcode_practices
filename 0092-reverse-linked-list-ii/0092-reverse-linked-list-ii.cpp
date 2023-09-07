cce3ab66f0ec6c559e69c7aa755bd2263c821fb1
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-09-07 12:42:44 +0600

    Time: 2 ms (52.03%), Space: 7.4 MB (89.74%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -17,18 +17,32 @@ public:
         
         ListNode* back = preHead;
         
+        //setting the previous to exact point
         for(int i=0;i<left-1; i++){
             back = back->next;
         }
-        cout<<back->val<<endl;
-        
+        //setting current
         ListNode* curr = back->next;
         
+        //we only need right-left iteration,
+        //each iteration one element should placed ok
         for(int i=0; i<right-left; i++){
+            //store these because now all are going to be lost
             ListNode* front = curr->next;
-            curr->next = front->next;
-            front->next = back->next;
+            ListNode* backNext = back->next;
+            ListNode* frontNext = front->next;
+            
+            
+            //last of the three node is came to first
             back->next = front;
+            
+            //after came to the second, front need to attach to current which is stored
+            //inside the backNext
+            front->next = backNext;
+            
+            //then attach the current to the place where previously front was
+            curr->next = frontNext;
+            
         }
         
         return preHead->next;
