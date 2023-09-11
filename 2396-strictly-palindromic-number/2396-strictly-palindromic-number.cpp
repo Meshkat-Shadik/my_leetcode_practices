d3507ed85e005a516c4c69ea96814059a13ba6d8
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-09-11 15:14:30 +0600

    Time: 0 ms (100.00%), Space: 6.2 MB (22.19%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -1,6 +1,28 @@
 class Solution {
+string decimalToBase(int num ,int i) {
+    stack<char> result;
+    while (num > 0) {
+        int rem = num % i;
+        char digit = '0' + rem;
+        result.push(digit);
+        num /= i;
+    }
+    string baseNum;
+    while (!result.empty()) {
+        baseNum += result.top();
+        result.pop();
+    }
+    return baseNum;
+}
 public:
     bool isStrictlyPalindromic(int n) {
-        return 0;
+        for(int i=2;i<=n-2;i++){
+            string s=decimalToBase(n,i);
+            string org=s;
+            reverse(s.begin(),s.end());
+            if(org!=s)
+                return false;
+        }  
+        return true;  
     }
 };
