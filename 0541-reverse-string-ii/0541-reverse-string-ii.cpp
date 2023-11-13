db9626ee7a6a68a782c8205fab91113a791aad10
Shadik Faysal <shadik14@cse.pstu.ac.bd>
2023-11-13 18:44:02 +0600

    Time: 3 ms (88.29%), Space: 7.6 MB (56.81%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+           int n = s.size();
+        for(int i=0; i<n; i+=2*k){
+            if(i+k > n)
+                reverse(s.begin()+i, s.end());
+            else
+                reverse(s.begin()+i, s.begin()+i+k);
+        }
+        return s;
+    }
+};
